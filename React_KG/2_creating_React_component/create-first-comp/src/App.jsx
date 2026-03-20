b453d15f8365abd4a9edd4fa92b9063c7b06ff3d
Srishti <ssrishti8968@gmail.com>
2026-03-20 15:38:00 +0530

    react learning

---
+++ React_KG/2_creating_React_component/create-first-comp/src/App.jsx
@@ -0,0 +1,15 @@
+import KgButton from "./kgButton";
+
+function App(){
+  return <div>
+  <h1>
+    Hello World
+    this is first react code
+  </h1>
+    <KgButton></KgButton>
+  </div>
+  
+  
+ 
+}
+export default App;
---
+++ React_KG/2_creating_React_component/create-first-comp/src/KgButton.jsx
@@ -0,0 +1,4 @@
+function KgButton(){
+    return <button>Like this Code</button>
+}
+export default KgButton;
